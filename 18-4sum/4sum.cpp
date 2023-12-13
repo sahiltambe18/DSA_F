4d0ddbff8a22a64f73b050e4b223f4bd390a70f3
sahil <75826199+sahiltambe18@users.noreply.github.com>
2023-12-13 14:19:50 +0530

    Time: 61 ms (56.84%) | Memory: 13.9 MB (37.28%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -8,9 +8,10 @@ public:
 
 
         for(int i = 0 ; i < n-3 ; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n - 2; ++j) {
             
-             for (int j = i + 1; j < n - 2; ++j) {
-            
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
                  int l = j+1 ;
                  int r = n-1;
             
@@ -19,6 +20,8 @@ public:
 
                     if(sum == target){
                         set.insert({nums[i] , nums[j] ,nums[l] ,nums[r]});
+                        while(l< r && nums[l]==nums[l+1]) l++;
+                        while(l< r && nums[r]==nums[r-1]) r--;
                         l++;
                         r--;
                     }else if(sum<target){
