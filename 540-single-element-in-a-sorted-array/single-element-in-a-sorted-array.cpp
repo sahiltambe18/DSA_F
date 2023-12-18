14dd0495cd1f217d5b8b5e693b4bf81043c81cee
sahil <75826199+sahiltambe18@users.noreply.github.com>
2023-12-18 18:41:47 +0530

    Time: 21 ms (43.60%) | Memory: 22.7 MB (22.73%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -4,20 +4,14 @@ public:
         int l = 0 , r = nums.size()-1;
         while(l<r){
             int mid = l +(r-l)/2;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
-                if(mid%2==0){
+                if(mid%2!=0){
+                    mid--;
+                }
                     if(nums[mid+1]==nums[mid]){
                         l = mid+2;
                     }else{
                         r = mid;
                     }
-                }else{
-                    if(nums[mid]==nums[mid-1]){
-                        l=mid+1;
-                    }else{
-                        r = mid-1;
-                    }
-                }
         }
         return nums[l];
     }
