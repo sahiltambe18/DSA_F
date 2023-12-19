0819c9c80305877504c71322dbdb3e4ead087da9
sahil <75826199+sahiltambe18@users.noreply.github.com>
2023-12-19 18:12:49 +0530

    Time: 41 ms (39.42%) | Memory: 19.5 MB (10.25%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    bool ispossible(vector<int>& piles, int h , int speed){
+        int time = 0;
+        for(auto i : piles){
+            time += (i+ speed-1) / speed;
+        }
+        return (time<=h);
+    }
+    /// break when time>h
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(piles.begin(),piles.end());
+        int l = 1 , r = piles[piles.size()-1];
+        while(l<r){
+            int mid = l + (r-l)/2;
+            if(ispossible(piles,h,mid)){
+                r = mid;
+            }else{
+                l = mid+1;
+            }
+        }
+        return l; 
+    }
+};
