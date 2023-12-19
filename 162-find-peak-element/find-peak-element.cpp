242974db36796f792c8c54461d4561883636e5da
sahil <75826199+sahiltambe18@users.noreply.github.com>
2023-12-19 12:37:14 +0530

    Time: 3 ms (66.45%) | Memory: 9.5 MB (13.21%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -11,10 +11,11 @@ public:
         while(l<r){
             int mid = l + (r-l)/2;
 
-            if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == n - 1 || nums[mid] > nums[mid + 1])) {
-                return mid;
-            } 
-            else if (nums[mid] < nums[mid + 1]) {
+            // if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == n - 1 || nums[mid] > nums[mid + 1])) {
+            //     return mid;
+            // } 
+            // else 
+            if (nums[mid] < nums[mid + 1]) {
                 l = mid + 1;
             } else {
                 r = mid;
