c1e8ba22b5a8db3b6a46f033eecd6413da7e76ca
sahil <75826199+sahiltambe18@users.noreply.github.com>
2023-12-21 19:19:18 +0530

    Time: 28 ms (47.53%) | Memory: 89.7 MB (70.52%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> arr;
-
-        int l1 = nums1.size();
-        int l2 = nums2.size();
-
-        // if(l1>l2){
-        //     return findMedianSortedArrays(nums2,nums1);
-        // }
-
-        int t1 = 0 , t2 = 0;
-        while(t1<l1  && t2<l2){
-            if(nums1[t1]<=nums2[t2]){
-                arr.push_back(nums1[t1]);
-                t1++;
+        int n = nums1.size() + nums2.size();
+        int i = 0 , j = 0 , c = 0;
+        int e1 = 0 , e2 = 0;
+        while((i<nums1.size() || j<nums2.size())&&c<(n/2)+1){
+            if(i<nums1.size() && j<nums2.size()){
+                if(nums1[i]<nums2[j]){
+                    e2 = e1 ;
+                    e1 = nums1[i];
+                    i++;
+                }else{
+                    e2 = e1 ;
+                    e1 = nums2[j];
+                    j++;
+                }
+                c++;
+            }else if(i<nums1.size()){
+                e2 = e1 ;
+                e1 = nums1[i];
+                i++;
+                c++;
             }else{
-                arr.push_back(nums2[t2]);
-                t2++;
+                e2 = e1 ;
+                e1 = nums2[j];
+                j++;
+                c++;
             }
         }
-        
-            while(t1<l1){
-                arr.push_back(nums1[t1]);
-                t1++;
-            }
-        
-            while(t2<l2){
-                arr.push_back(nums2[t2]);
-                t2++;
-            }
-        
 
-        if(arr.size()%2==0){
-            int mid = arr.size()/2;
-            return (arr[mid]+arr[mid-1])/2.0;
+        if(n%2==0){
+            return ((double)e1 + (double)e2)/2.0;
         }else{
-            int mid = arr.size()/2;
-            return arr[mid];
+            return (double)e1; 
         }
 
     }
