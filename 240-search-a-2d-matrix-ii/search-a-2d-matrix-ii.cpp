aedede3601eb8dca43468ba2f0911f9fba1117ec
sahil <75826199+sahiltambe18@users.noreply.github.com>
2023-12-21 21:30:59 +0530

    Time: 37 ms (99.07%) | Memory: 14.5 MB (98.71%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -2,15 +2,17 @@ class Solution {
    
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+     std::ios::sync_with_stdio(false);
      int rows = matrix.size() ;
      int i = 0 , j = matrix[0].size()-1;
     while(i<rows && j>=0){
-        if(matrix[i][j]==target) return true;
+        // last ele of row is large so decrese it
+        if(matrix[i][j]>target) j--;
         // last element is smaller than t ie not in this row
         else if(matrix[i][j]<target) i++; 
-        // last ele of row is large so decrese it
-        else j--;
+        else return true;
     }
+     matrix.clear();
      return false;
     }
 };
