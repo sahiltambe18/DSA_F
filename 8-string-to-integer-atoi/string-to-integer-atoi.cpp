2d758ef2fad7f85164e839fe9e7a6428e5509023
sahil <75826199+sahiltambe18@users.noreply.github.com>
2023-12-25 12:56:37 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (97.76%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        long long ans = 0;
-        bool flag = false, st = false;
-        for (char curr : s) {
-            if (curr == ' ' && !st) continue; // Skip space
+        long ans = 0;
+        int i = 0;
+        bool flag = false;
+        
+        while(s[i]==' ') i++;
+        if(s[i]=='-'){
+            flag = true;
+            i++;
+        }else if(s[i]=='+') i++;
 
-            if (isdigit(curr)) {
-                ans = ans * 10 + (curr - '0');
-                st = true;
-
-                if (ans > INT_MAX) return flag ? INT_MIN : INT_MAX; // Overflow 
-            } else if (curr == '-' && !st) {
-                flag = true;
-                st = true;
-            } else if (curr == '+' && !st) {
-                st = true;
-            } else {
-                break; 
+        while(i<s.length()){
+            char curr = s[i];
+            if(curr>='0' && curr<='9'){
+                ans = ans*10 + (curr-'0');
+                if(ans>INT_MAX){
+                    return flag ? INT_MIN : INT_MAX;
+                }
+            }else{
+                break;
             }
+            i++;
         }
         
         return flag ? -ans : ans;
+        
     }
 };
