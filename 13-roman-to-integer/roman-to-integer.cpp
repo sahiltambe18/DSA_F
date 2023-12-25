7ce926572cc358ab8f56464a6d383d9161a80822
sahil <75826199+sahiltambe18@users.noreply.github.com>
2023-12-25 12:24:23 +0530

    Time: 19 ms (14.04%) | Memory: 8.6 MB (35.74%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<char,int> mp = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
         int ans = 0,n = s.length();
         for(int i = 0 ; i < n ; i++){
-            if(i<n-1 && mp[s[i]]<mp[s[i+1]]){
+            if( mp[s[i]]<mp[s[i+1]]){
                 ans -= mp[s[i]];
             }else{
                 ans+=mp[s[i]];
