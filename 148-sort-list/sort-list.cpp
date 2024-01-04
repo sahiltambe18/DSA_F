09b7eee4be411125370f4ca4614918b414911fda
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-01-04 06:43:34 -0800

    Time: 143 ms (56.43%) | Memory: 74.3 MB (30.86%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -9,21 +9,48 @@
  * };
  */
 class Solution {
+    ListNode* midd(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+
+    ListNode* merge(ListNode* l1 , ListNode* l2){
+        ListNode* temp = new ListNode(0);
+        ListNode* newHead = temp;
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                temp->next = l1;
+                temp = l1;
+                l1 = l1->next;
+            }else{
+                temp->next = l2;
+                temp = l2;
+                l2 = l2->next;
+            }
+        }
+        temp->next = (l1==nullptr)?(l2):(l1);
+        return newHead->next; 
+    }
 public:
     ListNode* sortList(ListNode* head) {
-        ListNode *i = head;
-        vector<int> v ;
-        while(i){
-            v.push_back(i->val);
-            i = i->next;
-        }
-        sort(v.begin(),v.end());
-        i = head;
-        for(auto j : v){
-            i->val = j;
-            i = i->next; 
-        }
+        if(!head || !head->next) return head;
         
-        return head;
+        // divide lists
+        ListNode* leftHead = head;
+        ListNode* mid = midd(head);
+        ListNode* rightHead = mid->next;
+        mid->next = nullptr;
+
+        // divide further recur
+        leftHead = sortList(leftHead);
+        rightHead = sortList(rightHead);
+        
+        // it is what it is \U0001fae1
+        return merge(leftHead , rightHead);
     }
 };
