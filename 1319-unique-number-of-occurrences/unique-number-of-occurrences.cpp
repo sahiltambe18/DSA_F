3d41ca990f146f1f845399b71a662186f42b8bb4
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-01-17 22:52:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.59%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        
+        sort(arr.begin(),arr.end());
+
+        vector<int> v;
+        for(int i = 0 ; i<arr.size() ; i++){
+            int c = 1;
+            while(i+1<arr.size() && arr[i]==arr[i+1]){
+                c++;
+                i++;
+            } 
+            v.push_back(c);
+        }
+
+        sort(v.begin(),v.end());
+        for(int i = 1 ; i < v.size() ; i++){
+            if(v[i]==v[i-1]) return false;
+        }
+        return 1;
+
+    }
+};
