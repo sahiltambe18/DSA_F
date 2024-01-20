b446d8f1754fe0ef8159f83ad89030b9d2c1e73d
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-01-20 11:08:50 +0530

    Time: 188 ms (97.07%) | Memory: 9.6 MB (5.34%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,34 +1,32 @@
 class Solution {
-    bool flag;
-    
-    void check(vector<vector<char>>& board, vector<vector<bool>>& v, string word, int n, int x, int y) {
-        if (x >= board.size() || y >= board[0].size() || x < 0 || y < 0 || v[x][y] == true) {
-            return;
+    bool check(vector<vector<char>>& board, string &word, int n, int x, int y) {
+        if (x >= board.size() || y >= board[0].size() || x < 0 || y < 0 || board[x][y]!=word[n]) {
+            return false;
         }
-        if (n == word.length() - 1 && word[n] == board[x][y]) {
-            flag = true;
-            return;
-        }
-        if (n >= word.length()) return;
+        if (n == word.length()-1) return true;
+        
+        
+        char og = board[x][y]; 
+        board[x][y] = '#';
 
-        if (word[n] == board[x][y]) {
-            v[x][y] = true;
-            check(board, v, word, n + 1, x, y + 1);
-            check(board, v, word, n + 1, x + 1, y);
-            check(board, v, word, n + 1, x, y - 1);
-            check(board, v, word, n + 1, x - 1, y);
-            v[x][y] = false;  //  backtrack
-        }
+        bool res = check(board, word, n + 1, x + 1, y) || 
+                     check(board, word, n + 1, x - 1, y) || 
+                     check(board, word, n + 1, x, y + 1) || 
+                     check(board, word, n + 1, x, y - 1);
+        board[x][y] = og;
+        
+        return res;
     }
+
+
 
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<bool>> v(board.size(), vector<bool>(board[0].size(), false));
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board[0].size(); j++) {
-                flag = false;  
-                check(board, v, word, 0, i, j);
-                if (flag) return true;
+                if(check(board, word, 0, i, j)){
+                    return true;
+                }
             }
         }
         return false;
