c8878a494ea3960388212b24c49e82857667524f
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-01-24 16:56:41 +0530

    dy graph

--- tsp_bk.cpp
+++ tsp_bk.cpp
@@ -5,36 +5,48 @@ using namespace std;
 
 vector<int> ans;
 
-int graph[10][10] = {
-    {0, 10, 15, 20},
-    {10, 0, 35, 25},
-    {15, 35, 0, 30},
-    {20, 25, 30, 0}
-};
+// int graph[10][10] = {
+//     {0, 10, 15, 20},
+//     {10, 0, 35, 25},
+//     {15, 35, 0, 30},
+//     {20, 25, 30, 0}
+// };
+
+vector<vector<int>> graph;
 
 int minCost = INT_MAX;
 int n;
 
-bool all(const vector<bool>& v) {
-    for (auto i : v) {
-        if (!i) {
+// to check if all vertices are visited
+bool allVisited(const vector<bool> &v)
+{
+    for (auto i : v)
+    {
+        if (!i)
+        {
             return false;
         }
     }
     return true;
 }
 
-void tsp(vector<bool>& v, int cost, int c, vector<int>& path) {
-    if (all(v)) {
-        if (cost < minCost) {
-            minCost = cost;
+void tsp(vector<bool> &v, int cost, int c, vector<int> &path)
+{
+    if (allVisited(v))
+    {
+        if (cost + graph[c][0] < minCost)
+        {
+            minCost = cost + graph[c][0];
             ans = path;
+            ans.push_back(0);
         }
         return;
     }
 
-    for (int i = 0; i < n; i++) {
-        if (!v[i] && i!=c) {
+    for (int i = 0; i < n; i++)
+    {
+        if (!v[i] && i != c)
+        {
             v[i] = true;
             path.push_back(i);
             tsp(v, cost + graph[c][i], i, path);
@@ -44,27 +56,43 @@ void tsp(vector<bool>& v, int cost, int c, vector<int>& path) {
     }
 }
 
-int main() {
+int main()
+{
     cout << "Enter no of vertices\n";
     cin >> n;
-    
-    // Initialize the visited vector with false values
+
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> v(n, 0);
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j)
+            {
+                int e;
+                cout << "enter cost of " << i << " to " << j << endl;
+                cin >> e;
+                v[j] = e;
+            }
+        }
+        graph.push_back(v);
+    }
+
     vector<bool> v(n, false);
 
     // Start from vertex 0
     v[0] = true;
 
-    // Create an explicit vector<int> and pass it to the function
     vector<int> initialPath;
     initialPath.push_back(0);
     tsp(v, 0, 0, initialPath);
 
-    cout << "Min path: ";
-    for (auto i : ans) {
+    cout << "shortest path: ";
+    for (auto i : ans)
+    {
         cout << i << " ";
     }
-    cout<<endl;
-    cout << "Min path: "<<minCost;
+    cout << endl;
+    cout << "Min Cost: " << minCost;
 
     return 0;
 }
