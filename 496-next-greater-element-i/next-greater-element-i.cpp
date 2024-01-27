ad9587f40826c40ee1a2d69f2a469ad326d591e9
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-01-27 20:23:54 +0530

    Time: 9 ms (28.43%) | Memory: 11.3 MB (14.56%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -7,20 +7,11 @@ public:
         mp[n2[n2.size()-1]] = -1;
         st.push(n2[n2.size()-1]); 
         for(int i = n2.size()-2 ; i >=0 ; i--){
-            if(st.top() > n2[i]){
-                mp[n2[i]] = st.top();
-                st.push(n2[i]);
-            }else if(st.top()<n2[i]){
-                while(!st.empty() && st.top()<n2[i]){
-                    st.pop();
-                }
-                if(!st.empty()){
-                    mp[n2[i]] = st.top();
-                }else{
-                    mp[n2[i]] = -1;
-                }
-                    st.push(n2[i]);
+            while(!st.empty() && st.top()<n2[i]){
+                st.pop();
             }
+            mp[n2[i]] = st.empty() ? -1 : st.top();
+            st.push(n2[i]);
         }
         for(int i : n1){
             ans.push_back(mp[i]);
