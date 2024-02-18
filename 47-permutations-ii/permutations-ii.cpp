80544f3f8858ebaa90b763de775adfb6d69c6c2b
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-02-18 23:45:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (56.21%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
-    void bk(vector<int> &nums , set<vector<int>> &st , vector<int> &s , map<int,bool> &mp){
+    void bk(vector<int> &nums , vector<vector<int>> &ans , vector<int> &s , vector<bool> &used){
         if(s.size()==nums.size()){
-            st.insert(s);
+            ans.push_back(s);
             return;
         }
 
         for(int i = 0 ; i < nums.size();i++){
-            if(!mp[i]){
-                mp[i]=1;
+            if(used[i] || (i>0 && nums[i]==nums[i-1] && !used[i-1])) continue;
+
+            used[i] = true;
             s.push_back(nums[i]);
-            bk(nums,st,s,mp);
+            bk(nums,ans,s,used);
             s.pop_back();
-                mp[i]=0;
-            }
+            used[i] = false;
+
         }
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> s;
-        set<vector<int>> st;
-        map<int,bool> mp;
-        bk(nums,st,s,mp);
-        for(auto i : st){
-            ans.push_back(i);
-        }
+        vector<bool> used(nums.size(), false);
+        sort(nums.begin(), nums.end());
+        bk(nums,ans,s,used);
+
         return ans;
     }
 };
