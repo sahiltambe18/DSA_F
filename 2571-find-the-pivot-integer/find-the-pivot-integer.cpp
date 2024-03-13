d1b6b00b889842a77d4b46c0bf67441f4ad78604
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-03-13 16:22:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     int pivotInteger(int n) {
         if (n==1) return 1;
-        int sum =  ((n*n)+n)/2;
-        int rem = 0;
+        
         for(int i = 1 ; i < n ; i++){
-            rem+=i;
-            if(sum==rem) return i;
-            sum-=i;
+            if( i*(i+1) == (n+i)*(n-i+1) ) return i;
         }
         return -1;
     }
