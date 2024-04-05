69c15d59fcccdecbacc8922fec43c222e1885c9d
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-04-05 16:43:12 +0530

    Time: 10 ms (90.74%) | Memory: 20.1 MB (57.30%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,41 +1,17 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        vector<int> candies(n, 1); // Initialize candies for all children with 1
-
-        // Iterate until no further adjustments are needed
-        bool adjusted = true;
-        while (adjusted) {
-            adjusted = false;
-
-            // Traverse the ratings array from left to right
-            for (int i = 1; i < n; ++i) {
-                if (ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
-                    // If the current child has a higher rating than its left neighbor
-                    // and doesn't have more candies than its left neighbor, update candies
-                    candies[i] = candies[i - 1] + 1;
-                    adjusted = true;
-                }
-            }
-
-            // Traverse the ratings array from right to left
-            for (int i = n - 2; i >= 0; --i) {
-                if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
-                    // If the current child has a higher rating than its right neighbor
-                    // and doesn't have more candies than its right neighbor, update candies
-                    candies[i] = candies[i + 1] + 1;
-                    adjusted = true;
-                }
-            }
+        int size = ratings.size();
+        if (size <= 1) return size;
+        vector<int> num(size, 1);
+        for (int i = 1; i < size; i++) {
+            if (ratings[i] > ratings[i - 1])
+                num[i] = num[i - 1] + 1;
         }
-
-        // Calculate the total number of candies
-        // int total = 0;
-        // for (int candy : candies) {
-        //     total += candy;
-        // }
-
-        return accumulate(candies.begin(),candies.end(),0);
+        for (int i = size - 1; i > 0; i--) {
+            if (ratings[i - 1] > ratings[i])
+                num[i - 1] = max(num[i] + 1, num[i - 1]);
+        }
+        return accumulate(num.begin(), num.end(), 0);
     }
 };
