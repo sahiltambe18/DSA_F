b5eba822f9099c603eab39a1149cfdc33bbf628e
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-04-09 11:36:45 +0530

    Time: 4 ms (41.19%) | Memory: 9.5 MB (41.74%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,23 +1,16 @@
 #define arr(a) a.begin() , a.end()
 class Solution {
 public:
-    int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int target  = tickets[k];
-        int time = 0;
+    int timeRequiredToBuy(vector<int>& tik, int k) {
+        int target  = tik[k];
+        int time = 0 , n =  tik.size();
         queue<pair<int,bool>> q;
-        for(int i = 0 ; i < tickets.size() ; i++){
-            q.push({tickets[i],i==k});
+        for(int i = 0 ; i <=k ; i++){
+            time+=min(target,tik[i]);
         }
-
-        while(!q.empty()){
-            pair<int,bool> p = q.front();
-            q.pop();
-            p.first--;
-            time++;
-            if(p.second && p.first==0){
-                return time;
-            } 
-            if(p.first>0) q.push(p);
+        target--;
+        for(int i = k+1 ; i<n ; i++){
+            time+=min(target,tik[i]);
         }
 
         return time;
