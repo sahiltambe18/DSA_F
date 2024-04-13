3651bf41c20386e7f257f00f861ed5a338ce27fc
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-04-13 09:22:43 +0530

    Time: 62 ms (59.96%) | Memory: 22.2 MB (93.04%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,49 +1,30 @@
 class Solution {
-    int mx = 0;
-    int solve(vector<vector<char>>& matrix, int x, int j,
-              vector<vector<int>>& dp) {
-        if (x >= matrix.size() || j >= matrix[0].size()) {
-            return 0;
-        }
-        if (dp[x][j] != -1)
-            return dp[x][j];
-        int right = solve(matrix, x, j + 1, dp);
-        int diagonal = solve(matrix, x + 1, j + 1, dp);
-        int down = solve(matrix, x + 1, j, dp);
-        if (matrix[x][j] == '1') {
-            int ans = 1 + min(min(right, diagonal), down);
-            mx = max(mx, ans);
-            dp[x][j] = ans;
-            return ans;
-        } else {
-            dp[x][j] = 0;
-            return 0;
-        }
-    }
 
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
+        int mx = 0;
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        vector<int> curr(m + 1, 0), next(m + 1, 0);
         for (int x = n - 1; x >= 0; x--) {
             for (int j = m - 1; j >= 0; j--) {
 
-                
-                int right = dp[x][j+1];
-                int diagonal = dp[x+1][j+1];
-                int down = dp[x+1][j];
+                int right = curr[j + 1];
+                int diagonal = next[j + 1];
+                int down = next[j];
 
                 if (matrix[x][j] == '1') {
-                    dp[x][j] = 1 + min(min(right, diagonal), down);
-                    mx = max(mx,dp[x][j]);
+                    curr[j] = 1 + min(min(right, diagonal), down);
+                    mx = max(mx, curr[j]);
                 } else {
-                    dp[x][j] = 0;
+                    curr[j] = 0;
                 }
             }
+            next = curr;
         }
-        //solve(matrix, 0, 0, dp);
-        return mx*mx;
+        // solve(matrix, 0, 0, dp);
+        return mx * mx;
     }
 };
