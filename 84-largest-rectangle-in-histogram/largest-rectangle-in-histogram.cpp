1667c1b1d7abbc0d231dacf30857da44a895bffb
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-04-14 17:44:38 +0530

    Time: 115 ms (57.70%) | Memory: 77.8 MB (94.29%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,43 +1,31 @@
 class Solution {
-    vector<int> nxtSmaller(vector<int> &h ){
-        int n = h.size();
-        vector<int> ans(n);
-        stack<int> st;
-        st.push(-1);
-        for(int i = n-1 ; i>=0 ;i--){
-            while(st.top()!=-1 && h[st.top()]>=h[i]){
-                st.pop();
-            }
-            ans[i] = st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-
-    vector<int> prevSmaller(vector<int> &h){
-        int n = h.size();
-        vector<int> ans(n);
-        stack<int> st;
-        st.push(-1);
-        for(int i = 0 ; i <n ;i++){
-            while(st.top()!=-1 && h[st.top()]>=h[i]) st.pop();
-            ans[i] = st.top();
-            st.push(i);
-        }
-        return ans;
-    }
 public:
-    int largestRectangleArea(vector<int>& h) {
-        vector<int> prev , nxt;
-        nxt = nxtSmaller(h);
-        prev = prevSmaller(h);
-        int mx = 0 , n = h.size();
-        for(int i = 0 ; i < n ; i++){
-            if(nxt[i]==-1){
-                nxt[i] = n;
+    int largestRectangleArea(vector<int>&  h) {
+        int i = 0 , n = h.size();
+        int mx = INT_MIN;
+        stack<int> st;
+        while(i<n){
+            while(!st.empty() && h[st.top()]>= h[i]){
+                int idx = st.top();
+                st.pop();
+                if(st.empty()){
+                    mx = max(mx , h[idx]*i);
+                }else{
+                    mx = max(mx , h[idx]*(i-st.top()-1));
+                }
             }
-            int area = h[i]*(nxt[i]-prev[i]-1);
-            mx = max(mx,area);
+            st.push(i);
+            i++;
+        }
+
+        while(!st.empty()){
+            int idx = st.top();
+            st.pop();
+            if(st.empty()){
+                mx = max(mx , h[idx]*i);
+            }else{
+                mx = max(mx , h[idx]*(i-st.top()-1));
+            }
         }
         return mx;
     }
