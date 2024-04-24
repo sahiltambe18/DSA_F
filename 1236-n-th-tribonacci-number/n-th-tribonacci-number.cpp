d1987e33259621bc9ffa6e5f498b32d6092f15a9
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-04-25 00:57:36 +0530

    Time: 3 ms (22.77%) | Memory: 7.3 MB (29.12%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,18 +1,20 @@
 class Solution {
-    int solve(int n , unordered_map<int ,int> &mp){
-        if(mp.find(n)!=mp.end()){
-            return mp[n];
-        }
-
-        if(n==0) return 0;
-        if(n<=2) return 1;
-
-        mp[n] = solve(n-1,mp)+solve(n-2,mp)+solve(n-3,mp);
-        return mp[n];
-    }
 public:
+
+    
+
     int tribonacci(int n) {
-         unordered_map<int ,int> mp;
-        return solve(n,mp);
+         
+         if(n==0)return 0;
+         if(n<=2)return 1;
+
+         vector<int> dp(n+1 , 0);
+         dp[1] = 1;
+         dp[2] = 1;
+
+         for(int i = 3 ; i <= n ; i++){
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+         }
+         return dp[n];
     }
 };
