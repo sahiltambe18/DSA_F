435d189085d6942df1b64fa69bae180a2fa7764c
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-05-03 08:41:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (52.42%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -8,22 +8,18 @@ public:
         while (i1 < n1 || i2 < n2) {
             int val1 = 0, val2 = 0;
             
-            if (i1 < n1) {
-                auto dot1 = v1.find('.', i1);
-                if (dot1 ==  string::npos) dot1 = n1; 
-                val1 =  stoi(v1.substr(i1, dot1 - i1));
-                i1 = dot1 + 1;
+            while(i1 < n1 && v1[i1]!='.') {
+                val1 = val1*10+(v1[i1]-'0');
+                i1++;
             }
             
-            
-            if (i2 < n2) {
-                auto dot2 = v2.find('.', i2);
-                if (dot2 ==  string::npos) dot2 = n2; 
-                val2 =  stoi(v2.substr(i2, dot2 - i2));
-                i2 = dot2 + 1;
+            while(i2 < n2 && v2[i2]!='.') {
+                val2 = val2*10+(v2[i2]-'0');
+                i2++;
             }
-            
-            
+        
+            i1++;
+            i2++;
             if (val1 < val2) return -1;
             if (val1 > val2) return 1;
         }
