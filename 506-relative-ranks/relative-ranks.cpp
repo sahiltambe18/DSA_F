43d7f74b907aa0e38435d45fffe3926066422f3e
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-05-08 23:34:44 +0530

    Time: 7 ms (78.51%) | Memory: 13.8 MB (70.05%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        int n = score.size() , idx = 0;
-        vector<string> ans(n);
-        priority_queue<int> pq;
-        unordered_map<int,int> mp;
-        for(auto i : score){
-            pq.push(i);
-            mp[i] = idx;
-            idx++;
-        }
+        int n = score.size();
+        vector<pair<int, int>> rem;
+        vector<string> res(n);
 
-        idx = 0;
+        
+        for(int i = 0;i < n;i++)
+            rem.push_back({score[i], i});
 
-        while(idx < 3 && !pq.empty()){
-            if(idx==0){
-                ans[mp[pq.top()]] = "Gold Medal";
-            }else if(idx==1){
-                ans[mp[pq.top()]] = "Silver Medal";
-            }else{
-                ans[mp[pq.top()]] = "Bronze Medal";
-            }
-            pq.pop();
-            idx++;
-        }
 
-        if(idx==n){
-            return ans;
-        }
-        idx++;
-        while(!pq.empty()){
-                ans[mp[pq.top()]] = to_string(idx);
-                idx++;
-                pq.pop();
-        }
+        sort(rem.begin(), rem.end());
 
-        return ans;
+
+        for(int i = n-1;i >= 0;i--){
+            if(i == n-1)res[rem[i].second] = "Gold Medal";
+            else if(i == n-2)res[rem[i].second] = "Silver Medal";
+            else if(i == n-3)res[rem[i].second] = "Bronze Medal";
+            else res[rem[i].second] = to_string(n-i);
+        }
+        return res;
     }
 };
