593e84d44e6538200831b7f89f4b20674a9dade6
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-05-31 14:59:22 +0530

    Time: 5 ms (79.18%) | Memory: 12.2 MB (87.16%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -2,19 +2,28 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         
-        int n = nums.size();
-        unordered_map<int,int> mp;
-        vector<int> v;
+        // int n = nums.size();
+
+        int xorr = 0;
         for(auto i : nums){
-            if(mp[i]==1){
-                mp.erase(i);
+            xorr^=i;
+        }
+
+        int sep = 1;
+
+        while(!(xorr&sep)) sep = sep<<1;
+
+        int a = 0;
+        int b = 0;
+
+        for(auto i : nums){
+            if(sep&i){
+                a^=i;
             }else{
-                mp[i] = 1;
+                b^=i;
             }
         }
-        for(auto i : mp){
-            v.push_back(i.first);
-        }
-        return v;
+
+        return {a,b};
     }
 };
