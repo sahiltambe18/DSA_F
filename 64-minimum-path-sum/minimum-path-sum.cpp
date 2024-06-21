68b745f6aed3d45e98750d586654cb193155e76c
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-06-21 16:11:44 +0530

    Time: 3 ms (95.08%) | Memory: 12.1 MB (89.08%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -4,15 +4,16 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j<m ;j++){
-                if(i>0 && j>0){
-                    grid[i][j] +=  grid[i-1][j] > grid[i][j-1] ? grid[i][j-1] : grid[i-1][j];
-                }else if(i > 0){
-                    grid[i][j] += grid[i-1][j];
-                }else if(j>0){
-                    grid[i][j] += grid[i][j-1];
-                }
+        for(int i = 1 ; i < n ; i++){
+            grid[i][0] += grid[i-1][0];
+        }
+        for(int i = 1 ; i < m ; i++){
+            grid[0][i] += grid[0][i-1];
+        }
+
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 1 ; j<m ;j++){
+                grid[i][j] +=  grid[i-1][j] > grid[i][j-1] ? grid[i][j-1] : grid[i-1][j];
             }
         }
 
