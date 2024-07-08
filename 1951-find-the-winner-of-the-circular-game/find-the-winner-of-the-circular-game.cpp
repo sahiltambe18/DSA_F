f41b60afa1b4c7db046007bd5009be3597f6ef64
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-07-08 12:11:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (62.20%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,28 +1,10 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> v(n+1,0);
-        int rem = n;
-
-        int idx = 1;
-        while(rem>1){
-            int j = k;
-            while(j>1){
-                idx = (idx+1)%n;
-                if(v[idx]==0){
-                    j--;
-                }
-            };
-
-            v[idx] = 1;
-            rem--;
-            while(v[idx]!=0) idx = (idx+1)%n;
+        int winner = 0;  
+        for (int i = 2; i <= n; ++i) {
+            winner = (winner + k) % i;
         }
-        for(int i = 1 ; i <=n ;i++){
-            if(v[i]==0){
-                return i;
-            }
-        }
-        return -1;
+        return winner + 1;  
     }
 };
