d9487a0f734d7199c10e23dc0eb39244537942cd
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-09-10 16:45:23 +0530

    Time: 27 ms (97.17%) | Memory: 35.5 MB (49.64%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
@@ -9,21 +9,22 @@
  * };
  */
 class Solution {
-    int gcd(int a , int b){
-        int res = a > b ? b : a;
-        while(res>1){
-            if(a%res == 0 && b%res == 0) break;
-            res--;
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
-        return res;
+        return a;
     }
+
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
         ListNode* temp = head;
-        while(temp->next){
+        while (temp->next) {
             int a = temp->val;
             int b = temp->next->val;
-            int g = gcd(a,b);
+            int g = gcd(a, b);
             ListNode* n = new ListNode(g);
             n->next = temp->next;
             temp->next = n;
