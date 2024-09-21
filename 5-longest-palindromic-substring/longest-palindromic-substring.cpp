952a815410544dc08f56365c6822522fd7b54a6f
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-09-21 16:20:19 +0530

    Time: 8 ms (92.70%) | Memory: 13.2 MB (63.07%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -8,22 +8,17 @@ public:
         for(int i = 0 ; i < n-1 ; i++){
             // odd
             int l = i , r = i;
-            while(l>=0 && r < n && s[l]==s[r]){
-                l--,r++;
-            }
-            if(r-l-1 > ans.length()){
+            while(l>=0 && r<n && s[l]==s[r]) l--,r++;
+            if(r-l-1>ans.length()){
                 ans = s.substr(l+1,r-l-1);
             }
-
             if(s[i]==s[i+1]){
-                l = i , r = i+1;
-                while(l>=0 && r < n && s[l]==s[r]){
-                    l--,r++;
-                }
-                if(r-l-1 > ans.length()){
+                l=i,r=i+1;
+                while(l>=0 && r<n && s[l]==s[r]) l--,r++;
+                if(r-l-1>ans.length()){
                     ans = s.substr(l+1,r-l-1);
                 }
-
+                
             }
         }
         return ans;
