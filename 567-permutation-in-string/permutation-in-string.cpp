555fddb32678f718591fa94d963ac74ff93b3e27
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-10-05 12:43:32 +0530

    Time: 9 ms (48.50%) | Memory: 8.7 MB (80.36%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,43 +1,38 @@
 class Solution {
-    bool eq(vector<int> &mp , vector<int> &nmp ){
-        int l = 0 ; 
-        int r = 25;
-        while(l<=r){
-            if(mp[l]!=nmp[l] || mp[r]!=nmp[r]){
-                return false;
-            }
-            l++;
-            r--;
+    bool check(vector<int> &a, vector<int> &b){
+        for(int i = 0 ; i < 26 ;i++){
+            if(a[i]!=b[i]) return 0;
         }
-        return true;
+        return 1;
     }
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<int> mp(26,0) , nmp(26,0);
-
-        int k = s1.length() , n = s2.length() ;
-        if(k>n) return false;
+        if(s1.length()>s2.length()) return 0;
+        vector<int> f1(26,0) , f2(26,0);
+        for(char &i: s1){
+            f1[i-'a']++;
+        }
         int i = 0;
-
-        for( i ; i < k ; i++){
-            mp[s1[i]-'a']++;
-            nmp[s2[i]-'a']++;
+        int l = s1.size();
+        for(i = 0 ; i <l ; i++){
+            f2[s2[i]-'a']++;
         }
+        if(check(f1,f2)) return 1;
 
-        // check for eq
-        if(eq(mp,nmp)){
-            return true;
-        }
-        
-        for( i  ; i < n;i++){
-            nmp[s2[i]-'a']++;
-            nmp[s2[i-k]-'a']--;
-            // check eq
-            
-            if(eq(mp,nmp)){
-                return true;
+        l = s2.length();
+
+        int low = 0;
+
+        for(i ; i < l ; i++){
+            f2[s2[low++]-'a']--;
+            f2[s2[i]-'a']++;
+
+            if(f1[s2[i]-'a']==f2[s2[i]-'a']){
+                if(check(f1,f2)) return 1;
             }
         }
-        return false;
+
+        return 0;
+
     }
 };
