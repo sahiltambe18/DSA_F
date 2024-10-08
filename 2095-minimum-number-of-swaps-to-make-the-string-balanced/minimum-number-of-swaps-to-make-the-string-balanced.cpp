8a85351191666a14325365fd144c3e806c0c44fd
sahil <75826199+sahiltambe18@users.noreply.github.com>
2024-10-08 20:04:26 +0530

    Time: 107 ms (51.29%) | Memory: 32.7 MB (54.72%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        // stack<char> st;
+        int mx = 0 , swp = 0;
+        int open = 0 ;
+        int l = s.length()-1;
+        while(l>=0 && s[l]!='[') l--;
+        for(int i = 0 ; i < l ; i++){
+            if(s[i]==']') open++;
+            else{
+                open--;
+            }
+            if(mx<open){
+                swp++;
+                s[l] = ']';
+                s[i] = '[';
+                mx = open;
+                open--;
+            } 
+        }
+        return swp;
+    }
+};
